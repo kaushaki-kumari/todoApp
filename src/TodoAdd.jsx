378311927aa36f50aefd42cdc8732f725c00fb88
kaushaki-kumari <kaushaki.kumari@innotechteam.in>
2024-12-09 19:44:01 +0530

    work on validation for empty todo

--- src/TodoAdd.jsx
+++ src/TodoAdd.jsx
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
+import "./style.css";
+import className from 'classnames';
 function TodoAdd({ setTodos }) {
     const [inputValue, setInputValue] = useState("");
+    const [isInvalid, setInvalid] = useState(false);
 
     const handleInputChange = (value) => {
         setInputValue(value);
+        setInvalid(false);
     };
     const handleAddTodo = () => {
-        setTodos((prevTodos) => [
-            ...prevTodos,
-            { id: Date.now(), title: inputValue, completed: false },
-        ]);
-        setInputValue("");
+        const trimmedValue = inputValue.trim();
+        if (trimmedValue) {
+            setTodos((prevTodos) => [
+                ...prevTodos,
+                { id: Date.now(), title: inputValue, completed: false },
+            ]);
+            setInputValue(""); 
+            setInvalid(false);  
+        }
+        else {
+            setInvalid(true); 
+        }
     };
+
+    const inputClass = className(`form-control mb-3`,{
+        'is-invalid':isInvalid
+    });
+
+    const buttonClass = className("btn border border-2 btn-sm cursor-pointer");
     return (
         <div className="my-3">
             <h6>Todo</h6>
             <input
                 type="text"
-                className="form-control mb-3"
+                className={inputClass}
                 placeholder="Your Todo..."
                 value={inputValue}
                 onChange={(event) => handleInputChange(event.target.value)}
             />
             <button
                 type="button"
-                className="btn border border-2 btn-sm"
+                className={buttonClass}
                 onClick={handleAddTodo}
             >
                 Submit
